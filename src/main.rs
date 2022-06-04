78a8072a751e8f44f8c94ec55059eb0c0107bcfc
Pablo Takara <pablo.takara@glovoapp.com>
2022-06-04 20:29:41 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
@@ -9,5 +10,9 @@ async fn main() -> std::io::Result<()> {
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener)?.await
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    run(listener, connection_pool)?.await
 }
